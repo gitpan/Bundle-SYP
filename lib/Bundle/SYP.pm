6658b393c7446537fb8cb767d3197d09ace5a7c9
Станислав Пусеп <stas@sysd.org>
2011-06-30 16:16:02 +0000

    Import of SYP/Bundle-SYP-0.1 from CPAN.
    gitpan-cpan-distribution: Bundle-SYP
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         SYP/Bundle-SYP-0.1.tar.gz
    gitpan-cpan-author:       SYP
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/SYP.pm
@@ -0,0 +1,154 @@
+#!/usr/bin/perl
+package Bundle::SYP;
+# ABSTRACT: SYP's cozy environment
+
+use strict;
+our $VERSION = '0.1'; # VERSION
+
+1;
+
+
+__END__
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Bundle::SYP - SYP's cozy environment
+
+=head1 VERSION
+
+version 0.1
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::SYP'
+
+=head1 CONTENTS
+
+AnyEvent
+
+App::Ack
+
+App::cpanminus
+
+App::cpanoutdated
+
+DBD::SQLite
+
+DBI
+
+DB_File
+
+Data::Printer
+
+Devel::REPL
+
+Devel::REPL::Plugin::DataPrinter
+
+Digest::MD5
+
+Digest::SHA
+
+Dist::Zilla
+
+Dist::Zilla::Plugin::CopyFilesFromBuild
+
+Dist::Zilla::Plugin::Git::NextVersion
+
+Dist::Zilla::Plugin::MetaResourcesFromGit
+
+Dist::Zilla::Plugin::MinimumPerl
+
+Dist::Zilla::Plugin::OurPkgVersion
+
+Dist::Zilla::Plugin::PodWeaver
+
+Dist::Zilla::Plugin::ReadmeAnyFromPod
+
+Dist::Zilla::Plugin::VersionFromModule
+
+Dist::Zilla::PluginBundle::Git
+
+Dist::Zilla::PluginBundle::TestingMania
+
+EV
+
+Geo::Distance
+
+Geo::GoogleEarth::Pluggable
+
+HTML::TreeBuilder::XPath
+
+JSON::Any
+
+JSON::XS
+
+List::MoreUtils
+
+Log::Log4perl
+
+LWP::Simple
+
+Mojolicious
+
+MongoDB
+
+Moose
+
+Perl::Tags
+
+Pod::Elemental
+
+Pod::Simple
+
+Pod::Weaver::Plugin::Encoding
+
+Regexp::Common
+
+Storable
+
+String::Similarity
+
+String::Trigram
+
+Term::ReadLine::Gnu
+
+Text::CSV
+
+Text::CSV_XS
+
+Text::Unidecode
+
+Tie::Handle::CSV
+
+Tie::IxHash
+
+URI::Escape
+
+URI::QueryParam
+
+URI::sftp
+
+Unicode::CheckUTF8
+
+WWW::Curl
+
+common::sense
+
+lexicals
+
+=head1 AUTHOR
+
+Stanislaw Pusep <stas@sysd.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Stanislaw Pusep.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
